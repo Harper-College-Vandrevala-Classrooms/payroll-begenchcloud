f1aa9d7e41663d083fcdb3370eee2c21104e02ef
Ben <b_abdyvaidov@mail.harpercollege.edu>
2024-09-07 22:58:35 -0500

    Update Payroll.java
    finished the payroll app

--- src/main/java/com/csc/Payroll.java
+++ src/main/java/com/csc/Payroll.java
@@ -1,5 +1,68 @@
 package com.csc;
+import java.util.Scanner;
 
 public class Payroll {
-  // Implement your solution here!
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the Payroll App!!!");
+        System.out.println("");
+        System.out.print("Please enter number of hours worked this week: ");
+        int numberOfHoursWorked = scanner.nextInt();
+        System.out.print("Please enter number of dependants: ");
+        int numberOfDependants = scanner.nextInt();
+        scanner.close();
+        System.out.print("");
+
+        double grossPay;
+        if (numberOfHoursWorked <= 40) {
+
+            grossPay = numberOfHoursWorked * 16.78;
+
+        } else {
+
+            double overtime = numberOfHoursWorked - 40;
+            double beforeOvertime = 16.78 * 40;
+            double overtimePay = overtime * 25.17;
+            grossPay = beforeOvertime + overtimePay;
+        }
+
+        double ssTax = grossPay * 0.06;
+        double fiTax = grossPay * 0.14;
+        double siTax = grossPay * 0.05;
+        double unionDues = 10.00;
+        double insurance;
+
+        if (numberOfDependants > 2) {
+            insurance = 35.00;
+        } else {
+            insurance = 15.00;
+        }
+
+        double expenses = ssTax + fiTax + siTax + unionDues + insurance;
+        double netPay = grossPay - expenses;
+
+        System.out.println("Your payroll:");
+        System.out.println("=====================================");
+        System.out.println("Hours Worked: " + numberOfHoursWorked);
+        System.out.println("Number of dependants: " + numberOfDependants);
+        System.out.println("Hourly Rate: 16.78");
+        System.out.printf("Gross Pay: $%.2f\n", grossPay);
+        System.out.println("");
+        System.out.printf("Social Security Tax: $%.2f\n", ssTax);
+        System.out.printf("Federal Income Tax: $%.2f\n", fiTax);
+        System.out.printf("State Income Tax: $%.2f\n", siTax);
+        System.out.printf("Union Dues: $%.2f\n", unionDues);
+        System.out.printf("Insurance Cost: $%.2f\n", insurance);
+        System.out.printf("Total: $%.2f\n", expenses);
+        System.out.println("");
+        System.out.printf("Net Pay: $%.2f\n", netPay);
+        System.out.println("=====================================");
+        System.out.println("");
+        System.out.println("Thank you for using Payroll App!!!");
+        System.out.println("Bye");
+        System.exit(0);
+
+    }
 }
